0153822db70c860e1da65ed065c96f5f092ec35a
thebangtanepiphany <57727292+thebangtanepiphany@users.noreply.github.com>
2019-12-04 12:16:23 -0800

    initial commit
    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="header">
+        <h1>The Bangtan Epiphany Podcast</h1>
+      </section>
+      <section className="about-us">
+        <h2>About us</h2>
+        <p>BTS will blow your mind</p>
+      </section>
+      <section className="podcast-episodes">
+        <h2>Episodes</h2>
+      </section>
+    </div>
+  );
+}
+
+export default App;
