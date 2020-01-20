2ca137ec34c1ec108b7d237b58191e0ca9ab11e1
harder-than-that <56747295+harder-than-that@users.noreply.github.com>
2020-01-20 12:36:47 -0800

    added base content and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import twitter from "./twitter.svg"
 import twitterHover from "./twitter-hover.svg"
+import smicon from "./bangtan-icon-sm.png"
+import bangimg from "./bangtan-img.png"
+import em from "./emily.png"
+import rk from "./rocky.png"
 import "./App.css";
 
 function App() {
@@ -8,37 +12,70 @@ function App() {
   return (
     <div className="App">
       <header>
-        <ul>
-          <li>
-            <a href="#about-us">About us</a>
-          </li>
-          <li>
-            <a href="#episodes">Episodes</a>
-          </li>
-        </ul>
+        <nav className="navbar fixed-top">
+          <a className="navbar-brand" href="#">
+            <img className="" alt="The Bangtan Epiphany Podcast" src={smicon} />
+          </a>
+          <ul className="nav">
+          <li className="nav-item">
+              <a className="nav-link active" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#about-us">About us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#episodes">Episodes</a>
+            </li>
+          </ul>
+          <span className="nav-item">
+            <a href="https://www.twitter.com/btsepiphanypod" target="_blank" rel="noopener noreferrer">
+              <img 
+                className="social"
+                src={social}
+                alt="Twitter Icon"
+                onMouseEnter={() => setSocial(twitterHover)}
+                onMouseLeave={() => setSocial(twitter)}
+              />
+            </a>
+          </span>
+        </nav>
       </header>
-      <section className="header">
+      <section className="header container">
+        <div className="spacer"></div>
         <h1>The Bangtan Epiphany Podcast</h1>
+        <div class="card">
+          <img src={bangimg} className="card-img-top" alt="The Bangtan Epiphany Podcast" />
+          <div class="card-body">
+            <h3 class="card-title">Falling into BTS</h3>
+            <p class="card-text">Join married 30-somethings Rocky and Emily as we start to descend down the BTS rabbit hole, diving deep into the Bangtan Universe, exploring themes, culture, and stories created by this infectiously charming and surprisingly profound Korean idol group. We’ll review all the media and music, from debut till today, including their variety and reality shows, interviews, albums, mixtapes, social media, live shows, and MVs. We’re curious about culture, music, art, and storytelling and we’re opening our minds to the unexpectedly intense and endlessly delightful experience of being ARMY. </p>
+            <button>Podcast links here</button>
+          </div>
+        </div>
       </section>
-      <section className="about-us" id="about-us">
-        <h2>About us</h2>
-        <p>BTS will blow your mind</p>
+      <section className="about-us container" id="about-us">
+        <div className="spacer"></div>
+        <h2>About Us</h2>
+        <div className="card-deck">
+          <div className="card bg-light">
+            <img className="card-img-top about-img img-fluid image-responsive" alt="Emily" src={em} />
+            <div class="card-body">
+              <p class="card-text">Emily is a programmer, writer, and proud ARMY circa March 9, 2019 &#128521;. She loves to talk about culture, art, music... really just about anything. She may or may not be an immortal vampire.</p>
+            </div>
+          </div>
+          <div className="card bg-light">
+            <img className="card-img-top about-img img-fluid image-responsive" alt="Rocky" src={rk} />
+            <div class="card-body">
+              <p class="card-text">Rocky is a programmer, gamer and baby ARMY. He loves music and finding those deeper meanings.</p>
+            </div>
+          </div>
+        </div>
       </section>
-      <section className="podcast-episodes" id="episodes">
+      <section className="podcast-episodes container" id="episodes">
+        <div className="spacer"></div>
         <h2>Episodes</h2>
       </section>
       <footer>
-        <div>&copy; 2020</div>
-        <div className="social">
-          <a href="https://www.twitter.com/btsepiphanypod" target="_blank" rel="noopener noreferrer">
-            <img 
-              src={social}
-              alt="Twitter Icon"
-              onMouseEnter={() => setSocial(twitterHover)}
-              onMouseLeave={() => setSocial(twitter)}
-            />
-          </a>
-        </div>
+        <div>The Bangtan Epiphany Podcast &copy; 2020</div>
       </footer>
     </div>
   );
