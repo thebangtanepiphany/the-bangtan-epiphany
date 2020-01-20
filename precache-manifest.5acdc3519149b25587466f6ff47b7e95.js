6fa19630d2b9acef7a17abf6425de1f8a74da119
harder-than-that <56747295+harder-than-that@users.noreply.github.com>
2020-01-20 13:34:03 -0800

    Updates

---
+++ precache-manifest.5acdc3519149b25587466f6ff47b7e95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35dd00bdf0b2c70bd494ebc9eacf38c1",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/index.html"
+  },
+  {
+    "revision": "06cf2975c852a7573325",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ca43aee22d51c98e8f7f",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/css/main.897ddc23.chunk.css"
+  },
+  {
+    "revision": "06cf2975c852a7573325",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/js/2.f6e9c3eb.chunk.js"
+  },
+  {
+    "revision": "ca43aee22d51c98e8f7f",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/js/main.b33c346f.chunk.js"
+  },
+  {
+    "revision": "75f18d1f267e2eea2ab4",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/js/runtime-main.3a6ff61e.js"
+  },
+  {
+    "revision": "1fbf319c3e730e7c6ccb1a42f011e538",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/media/bangtan-img.1fbf319c.png"
+  },
+  {
+    "revision": "d7d3468bcd3a6a2eeaf8c0e88326a666",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/media/emily.d7d3468b.png"
+  },
+  {
+    "revision": "34f3e6afd73c60b1461c10d9ec6382a3",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/media/rocky.34f3e6af.png"
+  },
+  {
+    "revision": "c2f1acf6f29c52f793f66b65ba91d49f",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/media/twitter-hover.c2f1acf6.svg"
+  },
+  {
+    "revision": "b276254c4fea375e82cdcf542bf7b7ab",
+    "url": "/thebangtanepiphany/the-bangtan-epiphany/static/media/twitter.b276254c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40e7203baf107fdb05e5659807c4e378.js"
+  "/thebangtanepiphany/the-bangtan-epiphany/precache-manifest.5acdc3519149b25587466f6ff47b7e95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thebangtanepiphany/the-bangtan-epiphany/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
